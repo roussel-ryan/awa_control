d882c802917b7a5663f47d2cb0caa4bdb9e2ff31
Ryan Roussel <roussel@ucla.edu>
2021-08-13 14:38:54 -0500

    Added requirements and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='awa_control',
+    version='',
+    packages=['observations'],
+    url='',
+    license='',
+    author='Ryan Roussel',
+    author_email='rroussel@uchicago.edu',
+    description=''
+)
